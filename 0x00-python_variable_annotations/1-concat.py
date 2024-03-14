5e19fa6b792faa970efd209dd5d0cce8e92f913a
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-14 19:14:35 +0000

    solution of task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This file containa the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function takes in two strings and
+    returns a concatenated string"""
+    return str1 + str2
