64e5b60b0ef36b6379e04cfd01248451d98d656e
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-14 19:17:05 +0000

    solution task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This file contains the floor function"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """This function returns the floor of n"""
+    return math.floor(n)
