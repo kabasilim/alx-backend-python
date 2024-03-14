66bc3fc592e893b64661c56763ada9896f9fc87b
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-03-14 19:04:44 +0000

    0-

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This file contains the add function"""
+
+
+def add(a: float, b: float) -> float:
+    """This function takes in two floats
+    and returns their sum as a float
+    """
+    return a + b
